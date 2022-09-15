2ba6cb583182c202ae05806da208c599e2d7ac7b
Samet Vural <vuralasamet@gmail.com>
2022-09-15 23:30:37 +0300

    first commit

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// int ve double daha fazla kullanılır.
+		
+		double sayi = 12.5;
+		sayi = -129;
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = true;
+		
+	}
+
+}
